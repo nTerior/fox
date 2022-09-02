0efec4859ce720886ff58a4555b2852c8a162bde
cephox <paul-stier@gmx.de>
2022-09-02 16:43:55 +0200

    Fixed typos

--- include/io.h
+++ include/io.h
@@ -42,8 +42,8 @@ int can_write(char *path);
  * @brief Reads the given file
  *
  * @param path to the file
- * @param buffer pointer to the buffer, no initialisation needed. free() upon disposal
- * @param buffer_size pointer to the buffer size, no initialisation needed.
+ * @param buffer pointer to the buffer, no initialization needed. free() upon disposal
+ * @param buffer_size pointer to the buffer size, no initialization needed.
  * @return IO_OK on success, IO_DIRECTORY on file is actually a directory, IO_MISSING_PERMS on missing permissions
  */
 int read_file(char *path, unsigned char **buffer, unsigned long *buffer_size);
