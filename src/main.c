286e946647e94b9c7fdd154c0013eddfa714718c
cephox <paul-stier@gmx.de>
2022-09-02 16:40:13 +0200

    Better cleanup after end

--- src/main.c
+++ src/main.c
@@ -1,13 +1,22 @@
 #include "optparse.h"
 #include "ui.h"
 
+#include <stdlib.h>
+
 int quit(int key)
 {
   return 0;
 }
 
+void close()
+{
+  endwin();
+}
+
 int main(int argc, char **argv)
 {
+  atexit(close);
+
   struct fox_options options;
   parse_opts(argc, argv, &options);
 
@@ -15,8 +24,6 @@ int main(int argc, char **argv)
   ui_key_callback('q', quit, ui);
 
   ui_loop(ui);
-
-  endwin();
   ui_cleanup(ui);
 
   return 0;
